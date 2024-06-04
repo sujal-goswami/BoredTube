e82b5fcfed8eef07b54ee38e06203edbf5a52ca1
Sujal Goswami <sujalgoswamiofficial@gmail.com>
2024-06-04 19:43:05 +0530

    build database connection

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+/*
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.error("Error: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    }
+    catch (error) {
+        console.error("Error: ", error);
+        throw error;
+    }
+})()
+*/
