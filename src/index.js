d9a8c2be7cd8603badb590d4659db135aebc1b5f
Sujal Goswami <sujalgoswamiofficial@gmail.com>
2024-07-17 00:12:51 +0530

    Create all controllers files structure, completed all routers, completed video controller and add deleteOnCloudinary function in Cloudinary utils

--- src/index.js
+++ src/index.js
@@ -11,12 +11,8 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.on("error", (error) => {
-        console.log("Error: ", error);
-        throw error;
-    })
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+        console.log(` Server is running on port ${process.env.PORT}`);
     })
 })
 .catch((error) => {
