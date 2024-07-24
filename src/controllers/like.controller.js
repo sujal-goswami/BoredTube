2760edb83ce464dcaa9dd79ed6eb3d7e3014182d
Sujal Goswami <sujalgoswamiofficial@gmail.com>
2024-07-24 14:41:03 +0530

    completed like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,23 +6,163 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+    
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video Id");
+    }
+
+    const likedVideo = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id,
+    });
+
+    if (likedVideo) {
+        await Like.findByIdAndDelete(likedVideo?._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { isLiked: false}, "video disliked successfully"))
+    }
+
+    await Like.create({
+        video: videoId,
+        likedBy: req.user?._id,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }, "video liked successfully"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
-    //TODO: toggle like on comment
+        
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment Id");
+    }
+
+    const likedComment = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id,
+    });
+
+    if (likedComment) {
+        await Like.findByIdAndDelete(likedComment?._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { isLiked: false}, "comment disliked successfully"))
+    }
+
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }, "comment liked successfully"))
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+        
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet Id");
+    }
+
+    const likedTweet = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+    });
+
+    if (likedTweet) {
+        await Like.findByIdAndDelete(likedTweet?._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { isLiked: false}, "tweet disliked successfully"))
+    }
+
+    await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }, "tweet liked successfully"))
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id),
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                        },
+                    },
+                    {
+                        $unwind: "$ownerDetails",
+                    },
+                ],
+            },
+        },
+        {
+            $unwind: "$likedVideo",
+        },
+        {
+            $sort: {
+                createdAt: -1,
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                likedVideo: {
+                    _id: 1,
+                    "videoFile.url": 1,
+                    "thumbnail.url": 1,
+                    owner: 1,
+                    title: 1,
+                    description: 1,
+                    views: 1,
+                    duration: 1,
+                    createdAt: 1,
+                    isPublished: 1,
+                    ownerDetails: {
+                        username: 1,
+                        fullName: 1,
+                        "avatar.url": 1,
+                    },
+                },
+            },
+        },
+    ]);
+
+    if (!likedVideos) {
+        throw new ApiError(400, "Failed to fetch liked videos")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, likedVideos, "Liked videos fetched successfully"))
 })
 
 export {
