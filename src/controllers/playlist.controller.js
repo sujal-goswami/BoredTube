956050a08975faf76f18a8ebb60e5960a5ac2941
Sujal Goswami <sujalgoswamiofficial@gmail.com>
2024-07-25 16:07:31 +0530

    write create, delete, update playlist functions in playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -8,7 +8,23 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
-    //TODO: create playlist
+    if (!name || !description || name.trim() === "" || description.trim() === "") {
+        throw new ApiError(400, "Playlist name and decription is required");
+    }
+
+    const createdPlaylist = await Playlist.create({
+        name,
+        description,
+        owner: req.user?._id
+    })
+
+    if (!createdPlaylist) {
+        throw new ApiError(400, "Failed to create playlist !")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, createdPlaylist, "Playlist created successfully"))
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
@@ -33,13 +49,69 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    // TODO: delete playlist
+    
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist Id")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+    if (!playlist) {
+        throw new ApiError(400, "Playlist not found");
+    }
+
+    if (playlist.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(400, "Unauthorized access, you are not allowed to perform this action");
+    }
+
+    const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId);
+    if (!deletedPlaylist) {
+        throw new ApiError(400, "Failed to delete playlist");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Playlist deleted successfully"))
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
-    //TODO: update playlist
+    
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist Id")
+    }
+
+    if (!name || !description || name?.trim() == "" || description?.trim() == "") {
+        throw new ApiError(400, "Playlist name and description both are required")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+    if (!playlist) {
+        throw new ApiError(400, "Playlist not found")
+    }
+
+    if (playlist?.owner.toString() !== user?._id.toString()) {
+        throw new ApiError(403, "Unauthorized access, you are not allowed to perform this action")
+    }
+
+    const updatedPlayList = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set: {
+                name,
+                description
+            }
+        },
+        { new: true}
+    )
+
+    if (!updatedPlayList) {
+        throw new ApiError(400, "Failed to update playlist !")
+    }
+
+    return res 
+        .status(200)
+        .json(new ApiResponse(200, updatedPlayList, "Playlist updated successfully"))
 })
 
 export {
