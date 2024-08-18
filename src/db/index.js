6c36f81fc6cbf7ba11c3adf80e871984c7a65a78
Sujal Goswami <sujalgoswamiofficial@gmail.com>
2024-08-19 01:33:58 +0530

    Update MONGODB_URI to MONGODB_URL in .env.sample and src/db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log(`\n MONGODB connection successful !! DB HOST: ${connectionInstance.connections[0].host} \n`);
     } catch (error) {
         console.log("MONGODB_URI", process.env.MONGODB_URI)
